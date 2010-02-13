7c774f2e25490d7d6284781d373ae37268dc1a05
David Wolever <david@wolever.net>
2010-02-13 16:57:18 -0500

    Fixing up _bootstrap so it it explains what it's doing.

--- skel/setup/_bootstrap.sh
+++ skel/setup/_bootstrap.sh
@@ -1,21 +1,32 @@
 #!/usr/bin/env bash
 
-curl http://peak.telecommunity.com/dist/ez_setup.py > /tmp/ez_setup.py
-sudo python /tmp/ez_setup.py -U setuptools
-
-sudo easy_install virtualenv
-sudo easy_install virtualenvwrapper
-
-PYTHON_BASE=$(dirname `which python`)
-
-touch ~/.profile
-mkdir ~/.virtualenvs
-
-if [ ! "$(grep WORKON_HOME ~/.profile)"  ]
-    then echo "
-export WORKON_HOME=\$HOME/.virtualenvs
-source /usr/local/bin/virtualenvwrapper_bashrc
-" >> ~/.profile
+if [[ "`which easy_install`" == "" ]]
+then
+    echo "Using sudo to intsall easy_install..."
+    curl http://peak.telecommunity.com/dist/ez_setup.py > /tmp/ez_setup.py
+    sudo python /tmp/ez_setup.py -U setuptools
 fi
 
-source ~/.profile
+if [[ "`which workon`" == ""]]
+then
+    echo "Using sudo to install virtualenv and virtualenvwrapper (workon)"
+    sudo easy_install virtualenv
+    sudo easy_install virtualenvwrapper
+
+    PYTHON_BASE=$(dirname `which python`)
+
+    touch ~/.profile
+    mkdir ~/.virtualenvs
+
+    if [ ! "$(grep WORKON_HOME ~/.profile)"  ]
+        then echo "
+    export WORKON_HOME=\$HOME/.virtualenvs
+    source /usr/local/bin/virtualenvwrapper_bashrc
+    " >> ~/.profile
+    fi
+
+    source ~/.profile
+
+    echo "Insatlled."
+    echo "Your new WORKON_HOME is ~/.virtualenvs"
+fi
